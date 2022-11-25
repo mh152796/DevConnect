266753bc768cd734f67c4a4b18de611f8b2926eb
mosaraf-hossain <mosaraf15-2796@diu.edu.bd>
2022-11-25 12:14:55 +0600

    configured reviews and messages

--- users/forms.py
+++ users/forms.py
@@ -2,7 +2,7 @@ from dataclasses import field, fields
 from pyexpat import model
 from django.forms import ModelForm
 from django.contrib.auth.models import User
-from .models import Profile, Skill
+from .models import Profile, Skill, Message
 from django.contrib.auth.forms import UserCreationForm
 
 class CustomUserCreationFrom(UserCreationForm):
@@ -39,5 +39,15 @@ class SkillForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super(SkillForm, self).__init__(*args, **kwargs)
         
+        for name, field in self.fields.items():
+                field.widget.attrs.update({'class': 'input'})
+
+class MessageForm(ModelForm):
+    class Meta:
+        model = Message
+        fields = ['name', 'email', 'subject', 'body']
+    
+    def __init__(self, *args, **kwargs):
+        super(MessageForm, self).__init__(*args, **kwargs)
         for name, field in self.fields.items():
                 field.widget.attrs.update({'class': 'input'})
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate, logout
 from .models import Profile, Skill
 from django.db.models import Q
-from .forms import CustomUserCreationFrom,ProfileForm, SkillForm
+from .forms import CustomUserCreationFrom,ProfileForm, SkillForm, MessageForm
 from django.contrib import messages
 from .utils import searchProfiles, paginateProfiles
 from django.contrib.auth.decorators import login_required
@@ -125,3 +125,44 @@ def deleteSkill(request, pk):
         skill.delete()
         return redirect('account')
     return render(request, 'delete_template.html', context)
+@login_required(login_url='login')
+def inbox(request):
+    profile = request.user.profile
+    messageRequests = profile.messages.all()
+    unreadCount = messageRequests.filter(is_read=False).count()
+    context = {'messageRequests': messageRequests, 'unreadCount': unreadCount}
+    return render(request, 'users/inbox.html', context)
+
+def viewMessage(request, pk):
+    profile = request.user.profile
+    message = profile.messages.get(id=pk)
+    if message.is_read == False:   
+        message.is_read = True
+        message.save()
+    context = {'message': message}
+    return render(request, 'users/message.html', context)
+
+def createMessage(request, pk):
+    recipient = Profile.objects.get(id=pk)
+    form = MessageForm()
+    
+    try:
+        sender = request.user.profile
+    except: 
+        sender = None
+    
+    if request.method == 'POST':
+        form = MessageForm(request.POST) 
+        if form.is_valid():  
+           message = form.save(commit=False)
+           message.sender = sender
+           message.recipient = recipient
+           if sender:
+               message.name = sender.name
+               message.email = sender.email
+           message.save()
+           messages.success(request, 'Your message was successfully sent!')
+           return redirect('user-profile', pk=recipient.id)
+           
+    context = {'recipient': recipient, 'form': form}
+    return render(request, 'users/message_form.html', context)
