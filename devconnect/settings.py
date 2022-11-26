109e14bdb7e0461b1c1f54f2cf84110d8177cc32
mosaraf-hossain <mosaraf15-2796@diu.edu.bd>
2022-11-26 18:20:01 +0600

    sending welcome email and password reset

--- devconnect/settings.py
+++ devconnect/settings.py
@@ -114,8 +114,12 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
-
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'mosarofdiu123@gmail.com'
+EMAIL_HOST_PASSWORD = 'ehbnlyyngkagkkbu'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
--- users/signals.py
+++ users/signals.py
@@ -4,7 +4,8 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save, pre_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.decorators import login_required
-
+from django.core.mail import send_mail
+from django.conf import settings
 # @receiver(post_save, sender=User)
 def createProfile(sender, instance, created, **kwargs):
     if created:
@@ -14,6 +15,15 @@ def createProfile(sender, instance, created, **kwargs):
             username=user.username,
             email=user.email,
             name=user.first_name,
+        )
+        subject = 'Welcome to DevConnect'
+        message = 'We are glad you are here!'
+        send_mail(
+            subject,
+            message,
+            settings.EMAIL_HOST_USER,
+            [profile.email],
+            fail_silently=False
         )
         
 def updateUser(sender, instance, created, **kwargs):
